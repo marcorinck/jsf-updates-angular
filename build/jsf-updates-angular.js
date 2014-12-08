c359844479695c43e55dd5adfa75dbe24f9bc690
Marco Rinck <romulus23@gmail.com>
2014-12-08 08:50:04 +0100

    remove jquery dependency, completely untested for now

--- build/jsf-updates-angular.js
+++ build/jsf-updates-angular.js
@@ -1,11 +1,11 @@
 /**
- * jua - v0.1.0 - 2014-11-03
+ * jua - v0.1.0 - 2014-12-08
  * https://github.com/marcorinck/jsf-updates-angular
  * Copyright (c) 2014 Marco Rinck; Licensed MIT
  */
-(function (window, angular, jsf, document, $) {
+(function (window, document) {
   'use strict';
-  var onCompleteCallbacks = [], requestOngoing = false;
+  var angular, jsf, onCompleteCallbacks = [], requestOngoing = false;
   function escapeJSFClientId(id) {
     return '#' + id.replace(/:/g, '\\:');
   }
@@ -26,27 +26,34 @@
       onCompleteCallbacks.push(callback);
     }
   }
-  function destroyScopes(data) {
-    var updates = data.responseXML.getElementsByTagName('update');
-    $.each(updates, function (index, update) {
-      var id = escapeJSFClientId(update.id);
-      if (!id.contains('ViewState')) {
-        $(id).find('.ng-scope, .ng-isolate-scope').each(function (index, scopedChildElement) {
-          if (window.jua.debug) {
-            console.log('destroying child scope for element', scopedChildElement);
-          }
-          angular.element(scopedChildElement.firstChild).scope().$destroy();
-        });
+  function destroyScopedElements(elementsWithScope) {
+    var i, length;
+    if (elementsWithScope && elementsWithScope.length) {
+      length = elementsWithScope.length;
+      for (i = 0; i < length; i++) {
+        if (window.jua.debug) {
+          console.log('destroying child scope for element', elementsWithScope[i]);
+        }
+        angular.element(elementsWithScope[i].firstChild).scope().$destroy();
       }
-    });
+    }
+  }
+  function destroyScopes(data) {
+    var updates = data.responseXML.getElementsByTagName('update'), length = updates.length, i, id;
+    for (i = 0; i < length; i++) {
+      id = escapeJSFClientId(updates[i].id);
+      if (!id.contains('ViewState')) {
+        destroyScopedElements(document.querySelector(id).querySelectorAll('.ng-scope, .ng-isolate-scope'));
+      }
+    }
   }
   function handleAjaxUpdates(data) {
     window.setTimeout(function () {
-      var $compile = angular.element(document).injector().get('$compile'), updates = data.responseXML.getElementsByTagName('update');
-      $.each(updates, function (index, update) {
-        var id = escapeJSFClientId(update.id), element;
+      var $compile = angular.element(document).injector().get('$compile'), updates = data.responseXML.getElementsByTagName('update'), length = updates.length, i, id, element;
+      for (i = 0; i < length; i++) {
+        id = escapeJSFClientId(updates[i].id);
         if (!id.contains('ViewState')) {
-          element = angular.element($(id));
+          element = angular.element(document.querySelector(id));
           if (element) {
             if (window.jua.debug) {
               console.log('compiling angular element', element);
@@ -54,7 +61,7 @@
             $compile(element)(element.scope());
           }
         }
-      });
+      }
       if (onCompleteCallbacks.length) {
         onCompleteCallbacks.forEach(function (onCompleteCallback) {
           onCompleteCallback();
@@ -63,25 +70,33 @@
       }
     });
   }
-  jsf.ajax.addOnEvent(function (data) {
-    if (data.status === 'begin') {
-      requestOngoing = true;
-      onCompleteCallbacks = [];
+  jsf = window.jsf;
+  angular = window.angular;
+  if (jsf && angular) {
+    jsf.ajax.addOnEvent(function (data) {
+      if (data.status === 'begin') {
+        requestOngoing = true;
+        onCompleteCallbacks = [];
+      }
+      if (data.status === 'complete') {
+        destroyScopes(data);
+      }
+      if (data.status === 'success') {
+        handleAjaxUpdates(data);
+        requestOngoing = false;
+      }
+    });
+    window.jua = {
+      onComplete: onComplete,
+      onCompleteEvent: onCompleteEvent,
+      ensureExecutionAfterAjaxRequest: ensureExecutionAfterAjaxRequest,
+      get requestOngoing() {
+        return requestOngoing;
+      }
+    };
+  } else {
+    if (console && console.warn) {
+      console.warn('jsf-updates-angular: no jsf javascript or no angular found. Doing nothing.');
     }
-    if (data.status === 'complete') {
-      destroyScopes(data);
-    }
-    if (data.status === 'success') {
-      handleAjaxUpdates(data);
-      requestOngoing = false;
-    }
-  });
-  window.jua = {
-    onComplete: onComplete,
-    onCompleteEvent: onCompleteEvent,
-    ensureExecutionAfterAjaxRequest: ensureExecutionAfterAjaxRequest,
-    get requestOngoing() {
-      return requestOngoing;
-    }
-  };
-}(window, angular, jsf, document, jQuery));
+  }
+}(window, document));
---
+++ test/dummyTest.js
@@ -0,0 +1,11 @@
+(function() {
+	'use strict';
+
+	describe("Dummy", function() {
+
+		it('should work', function() {
+			expect(true).toBe(true);
+		});
+	});
+
+})();
